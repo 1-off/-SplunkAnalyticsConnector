73f158f2797e37bcabce3a3aedf32f97459c53ba
1-off <97915378+1-off@users.noreply.github.com>
2023-02-09 01:09:46 +0100

    Update content_script.js

--- content_script.js
+++ content_script.js
@@ -1,50 +1,21 @@
+document.body.style.border = "5px solid red";
 
+const input = document.querySelector("input[type='file']");
 
-let readFile = (_path) => {
-    return new Promise((resolve, reject) => {
-        fetch(_path, {mode:'same-origin'})
-            .then(function(_res) {
-                return _res.blob();
-            })
-            .then(function(_blob) {
-                var reader = new FileReader();
+input.addEventListener("change", function () {
+  // get the first file from the list of selected files
+  const file = this.files[0];
+  console.log("content script: file selected");
 
-                reader.addEventListener("loadend", function() {
-                    resolve(this.result);
-                });
-
-                let text = reader.readAsText(_blob);
-                console.log(text);
-            })
-            .catch(error => {
-                reject(error);
-            });
-    });
-};
-
-
-async function getListOfFilenamesInCurrentDirectory(curDirectory) {
-    try {
-        const response = await fetch(curDirectory,{mode:'same-origin'});
-        const data = await response.text();
-        const fileList = data.split("\n");
-        for (let line of fileList) {
-                // console.log(line.split(" ")[1]);
-                if(line.split(" ")[1] == "hostname.txt"){
-                    readFile(curDirectory+"/"+line.split(" ")[1]).then(_res => {
-                        console.log(_res); // <--  result (file content)
-                })
-        .catch(_error => {
-            console.log(_error );
-        });
-                }
-                
-        }
-    } catch (err) {
-        console.error(err.name); 
-        console.error(err.message); 
-        console.error(err.stack); 
-        console.error(err); 
-      }
-}
-
+let fileData = {
+    name: file.name,
+    lastModified: file.lastModified,
+    webkitRelativePath: file.webkitRelativePath,
+    size: file.size,
+    type: file.type,
+  }
+  // stringify the file data to create a JSON string
+  const jsonData = JSON.stringify(fileData, null, 2);
+  console.log(jsonData);
+  browser.runtime.sendMessage({ data: jsonData });
+});
